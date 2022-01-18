cad20be5af6fa1268bac8532b22aed4677a2e46f
Alexander Konovalov <shuryak@protonmail.com>
2022-01-18 16:35:31 +0300

    feat(tests): actualize tests in FollowersService

--- Friendbook.Tests/FollowersServiceTests.cs
+++ Friendbook.Tests/FollowersServiceTests.cs
@@ -24,13 +24,40 @@ public class FollowersServiceTests
     [TestCase(1, 2)]
     [TestCase(2, 3)]
     [TestCase(3, 2)]
-    public void Follow_ShouldReturnTrue(int followerId, int followingId)
+    public void Follow_IsNotFollowed_ShouldReturnTrue(int followerId, int followingId)
     {
         // Arrange
         _followerPairRepositoryMock
             .Setup(x => x.Create(followerId, followingId))
             .Verifiable();
-        
+
+        _followerPairRepositoryMock
+            .Setup(x => x.GetRelationshipStatus(followerId, followingId))
+            .Returns(RelationshipStatuses.IsNotFollowed);
+
+        // Act
+        bool result = _followersService.Follow(followerId, followingId);
+
+        // Assert
+        _followerPairRepositoryMock.Verify(x => x.Create(followerId, followingId), Times.Once);
+        Assert.IsTrue(result);
+    }
+    
+    [Test]
+    [TestCase(1, 2)]
+    [TestCase(2, 3)]
+    [TestCase(3, 2)]
+    public void Follow_IsFollowing_ShouldReturnTrue(int followerId, int followingId)
+    {
+        // Arrange
+        _followerPairRepositoryMock
+            .Setup(x => x.Create(followerId, followingId))
+            .Verifiable();
+
+        _followerPairRepositoryMock
+            .Setup(x => x.GetRelationshipStatus(followerId, followingId))
+            .Returns(RelationshipStatuses.IsFollowing);
+
         // Act
         bool result = _followersService.Follow(followerId, followingId);
 
@@ -71,6 +98,38 @@ public class FollowersServiceTests
 
         // Assert
         Assert.IsTrue(result1 != result2 && result2 == false);
+    }
+
+    [Test]
+    [TestCase(1, 2)]
+    public void GetRelationshipStatus_ShouldReturnValidRelationship(int followerId, int followingId)
+    {
+        // Arrange
+        _followerPairRepositoryMock.Setup(x => x.GetRelationshipStatus(followerId, followingId)).Verifiable();
+
+        // Act
+        RelationshipStatuses result = _followersService.GetRelationshipStatus(followerId, followingId);
+
+        // Assert
+        _followerPairRepositoryMock.Verify(x => x.GetRelationshipStatus(followerId, followingId), Times.Once);
+    }
+    
+    [Test]
+    [TestCase(1, 0)]
+    [TestCase(-5, 2)]
+    [TestCase(0, 0)]
+    [TestCase(-5, -5)]
+    public void GetRelationshipStatus_ShouldReturnInvalidRelationship(int followerId, int followingId)
+    {
+        // Arrange
+        _followerPairRepositoryMock.Setup(x => x.GetRelationshipStatus(followerId, followingId)).Verifiable();
+
+        // Act
+        RelationshipStatuses result = _followersService.GetRelationshipStatus(followerId, followingId);
+
+        // Assert
+        _followerPairRepositoryMock.Verify(x => x.GetRelationshipStatus(followerId, followingId), Times.Never);
+        Assert.IsTrue(result is RelationshipStatuses.InvalidRelationship);
     }
     
     [Test]
@@ -146,13 +205,40 @@ public class FollowersServiceTests
     [TestCase(1, 2)]
     [TestCase(2, 3)]
     [TestCase(3, 2)]
-    public void Unfollow_ShouldReturnTrue(int followerId, int followingId)
+    public void Unfollow_IsFollowed_ShouldReturnTrue(int followerId, int followingId)
     {
         // Arrange
         _followerPairRepositoryMock
             .Setup(x => x.Delete(followerId, followingId))
             .Verifiable();
-        
+
+        _followerPairRepositoryMock
+            .Setup(x => x.GetRelationshipStatus(followerId, followingId))
+            .Returns(RelationshipStatuses.IsFollowed);
+
+        // Act
+        bool result = _followersService.Unfollow(followerId, followingId);
+
+        // Assert
+        _followerPairRepositoryMock.Verify(x => x.Delete(followerId, followingId), Times.Once);
+        Assert.IsTrue(result);
+    }
+    
+    [Test]
+    [TestCase(1, 2)]
+    [TestCase(2, 3)]
+    [TestCase(3, 2)]
+    public void Unfollow_IsFriends_ShouldReturnTrue(int followerId, int followingId)
+    {
+        // Arrange
+        _followerPairRepositoryMock
+            .Setup(x => x.Delete(followerId, followingId))
+            .Verifiable();
+
+        _followerPairRepositoryMock
+            .Setup(x => x.GetRelationshipStatus(followerId, followingId))
+            .Returns(RelationshipStatuses.IsFriends);
+
         // Act
         bool result = _followersService.Unfollow(followerId, followingId);
 
@@ -162,19 +248,61 @@ public class FollowersServiceTests
     }
 
     [Test]
+    public void Unfollow_IsNotFollowed_ShouldReturnFalse()
+    {
+        // Arrange
+        _followerPairRepositoryMock.Setup(x => x.Delete(1, 2)).Verifiable();
+        _followerPairRepositoryMock
+            .Setup(x => x.GetRelationshipStatus(1, 2))
+            .Returns(RelationshipStatuses.IsNotFollowed);
+        
+        // Act
+        bool result = _followersService.Unfollow(1, 2);
+
+        //Assert
+        // _followerPairRepositoryMock.Verify(x => x.Delete(1, 2), Times.Never);
+        Assert.IsFalse(result);
+    }
+
+    [Test]
     [TestCase(-1, 1)]
     [TestCase(1, -1)]
     [TestCase(1, 0)]
-    public void Unfollow_ShouldReturnFalse(int followerId, int followingId)
+    public void Unfollow_InvalidArguments_ShouldReturnFalse(int followerId, int followingId)
     {
         // Arrange
         _followerPairRepositoryMock.Setup(x => x.Delete(followerId, followingId)).Verifiable();
-        
+
         // Act
         bool result = _followersService.Unfollow(followerId, followingId);
 
         //Assert
         _followerPairRepositoryMock.Verify(x => x.Delete(followerId, followingId), Times.Never);
         Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void Unfollow_TwiceFromOneUserProfile_ShouldReturnFalse()
+    {
+        // Arrange
+        _followerPairRepositoryMock
+            .SetupSequence(x => x.GetRelationshipStatus(1, 2))
+            .Returns(RelationshipStatuses.IsFollowed)
+            .Returns(RelationshipStatuses.IsNotFollowed);
+        
+        _followerPairRepositoryMock
+            .SetupSequence(x => x.GetRelationshipStatus(2, 3))
+            .Returns(RelationshipStatuses.IsFriends)
+            .Returns(RelationshipStatuses.IsFollowing);
+
+        // Act
+        bool result1 = _followersService.Unfollow(1, 2);
+        bool result2 = _followersService.Unfollow(1, 2);
+        bool result3 = _followersService.Unfollow(2, 3);
+        bool result4 = _followersService.Unfollow(2, 3);
+        
+        // Assert
+        Assert.IsTrue(result1 != result2 && result2 == false);
+        Assert.IsTrue(result3 != result4 && result4 == false);
     }
 }
